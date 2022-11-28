464f684b362c7a24de010c02f514e94c21b24d52
jamestaitgh <james@theartistinthegalleryhall.com>
2022-11-28 15:23:26 +0000

    Newer Bigger Picture

--- dist/scripts/script.js
+++ dist/scripts/script.js
@@ -1,54 +1,20 @@
 window.addEventListener("DOMContentLoaded", () => {
-  let {documentElement: html, body} = document
-  let defaultHtmlOverflow, defaultBodyOverflow
-
-  // BIG-PICTURE HIDESHOW SCROLL FUNCTIONS
-  let hideScroll = () => {
-    if (html.scrollHeight > html.clientHeight) {
-      // store existing overflow style
-      defaultHtmlOverflow =
-        defaultHtmlOverflow || getComputedStyle(html).overflowY
-      defaultBodyOverflow =
-        defaultBodyOverflow || getComputedStyle(body).overflowY
-      // hide overflow
-      html.style.overflowY = "hidden"
-      body.style.overflowY = "scroll"
-    }
-  }
-
-  let showScroll = (index) => {
-    // show overflow
-    html.style.overflowY = defaultHtmlOverflow
-    body.style.overflowY = defaultHtmlOverflow
-  }
-
-  // END BIG-PICTURE HIDESHOW SCROLL FUNCTIONS
-
-  const setOfImages = Array.from(document.querySelectorAll("#printwrap .print"))
-
-  setOfImages.map((element, index) => {
-    element.addEventListener("click", (e) => {
-      console.log(index)
-      e.preventDefault()
-      BigPicture({
-        el: e.target,
-        gallery: document.querySelectorAll("#printwrap .print"),
-        position: index,
-        loop: true,
-
-        // animationStart executed immediately before open animation starts
-        animationStart: hideScroll(index),
-        // animationEnd executed immediately after open animation finishes
-        animationEnd: function () {
-          console.log("it has opened")
-        },
-        // onClose executed immediately after close animation finishes
-        onClose: showScroll(index),
-        // onChangeImage executed on gallery image change
-        onChangeImage: function (props) {
-          console.log("gallery image changed", props)
-        },
-      })
-    })
+  const bp = BiggerPicture({
+    target: document.body,
   })
+
+  const setOfImages = document.querySelectorAll("#printwrap .print")
+
+  for (let link of setOfImages) {
+    link.addEventListener("click", openGallery)
+  }
+
+  // open BiggerPicture
+  function openGallery(e) {
+    e.preventDefault()
+    bp.open({
+      items: setOfImages,
+      el: e.currentTarget,
+    })
+  }
 })
